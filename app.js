e66492d90cdff230abc8f4cb83e6f06b9febeff9
oguzhanaydemir <ouzhanaydemir7@gmail.com>
2018-10-23 19:21:55 +0300

    chatRoute

--- app.js
+++ app.js
@@ -14,11 +14,28 @@ const db = require('./helpers/db')();
 //Routes
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
+const chatRouter = require('./routes/chat');
 
 const app = express();
 
+//Session
+const session = require('express-session');
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}
+
+app.use(session(sessionOptions));
+
 //Passport
 const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,10 +47,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
-app.use(passport.initialize());
+
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,3 +1,3 @@
-app.controller('indexController', ['$scope', ($scope) => {
-    console.log('indexController');
+app.controller('chatController', ['$scope', ($scope) => {
+    console.log('chatController');
 }]);
