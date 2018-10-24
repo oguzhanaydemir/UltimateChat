72b912ec0a27fb258cbab3f36d4419c15c86d522
oguzhanaydemir <ouzhanaydemir7@gmail.com>
2018-10-24 04:23:52 +0300

    socket authorization

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,18 +1,22 @@
 const socketio = require('socket.io');
 const io = socketio();
+const redisAdapter = require('socket.io-redis');
+const socketAuthorization = require('../middleware/socketAuthorization');
+
 
 const socketApi = {
     io
 }
 
-const redisAdapter = require('socket.io-redis');
+io.use(socketAuthorization);
+
 io.adapter(redisAdapter({
     host: process.env.REDIS_URI, 
     port: process.env.REDIS_PORT
 }));
 
 io.on('connection', (socket) => {
-    console.log('a user logged in');
+    console.log('a user logged in with name ', socket.request.user.name);
 
     socket.broadcast.emit('hello', { text: 'hello' });
 });
