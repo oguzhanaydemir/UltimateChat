0cf028f540b8ae1b075ac1d949cf5c68196dfbd2
oguzhanaydemir <ouzhanaydemir7@gmail.com>
2018-10-26 02:15:03 +0300

    edit chat-ui for online users tab

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,5 +1,10 @@
 app.controller('chatController', ['$scope', ($scope) => {
     const socket = io.connect('http://localhost:3000');
 
-   
+    $scope.activeTab = 2;
+
+    $scope.changeTab = tabValue => {
+        $scope.activeTab = tabValue;
+    }
+
 }]);
